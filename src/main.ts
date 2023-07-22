f50c3cb625b490e626a1f77d029ff5be278bbd61
dengxiaojun <dxj1718874198@gmail.com>
2023-07-22 20:00:16 +0800

    [update]Demo test page

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import APlayer from '@moefe/vue-aplayer';
+import APlayer from '@moefe/vue-aplayer-latest';
 Vue.use(<any>APlayer, {
   defaultCover: 'https://github.com/u3u.png',
   productionTip: false,
